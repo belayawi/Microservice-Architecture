c5ea59fb9b903e7015cb3b82c16760c521148895
Belay Buta <belaynehmulat@gmail.com>
2023-01-01 21:37:56 -0600

    command book created

---
+++ src/main/java/com/example/bookcommandservice/domain/Book.java
@@ -0,0 +1,67 @@
+package com.example.bookcommandservice.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Book {
+    @Id
+    private String isbn;
+    private String title;
+    private String description;
+    private String authorName;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String title, String description, String authorName) {
+        this.isbn = isbn;
+        this.title = title;
+        this.description = description;
+        this.authorName = authorName;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", authorName='" + authorName + '\'' +
+                '}';
+    }
+
+
+}
