25ace4c074a193aa14b2b6a59df394b2a68507df
Belay Buta <belaynehmulat@gmail.com>
2023-01-01 21:32:18 -0600

    first commit

---
+++ src/main/java/com/example/bookcommandservice/BookCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookcommandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCommandServiceApplication.class, args);
+	}
+
+}
